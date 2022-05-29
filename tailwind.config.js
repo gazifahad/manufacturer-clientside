06f93f26f9e6e1a4d0561e07239294be71ae3544
gazifahad <gazifahad3@gmail.com>
2022-05-29 21:04:54 +0600

    theme added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,10 @@ module.exports = {
   ],
   daisyui: {
     themes: [
-      {
-        'mytheme': { // custom theme
-          'primary' : '#7f75ae',
-          'primary-focus' : '#31c127',
-          'bg' :'#31c127',
-          'primary-content' : '#ffff00',
-          // other colors
-        },
-        'myothertheme': { // custom theme
-          'primary' : '#ffff00',
-          'primary-focus' : '#ffff00',
-          'primary-content' : '#ffff00',
-          // other colors
-        },
-      },
-      '', // and some pre-defined theme
-      'forest',
-      'synthwave'
+       // and some pre-defined theme
+      
+      
+      "light", "dark", "cupcake",
     ],
   },
   plugins: [
